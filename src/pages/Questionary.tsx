40526a86b0ec706a11e81fd8ff594bea44879780
Victor Alves Farias <victoralvesfarias2004@gmail.com>
2023-10-03 17:09:59 -0300

    Loading component added

--- src/pages/Questionary.tsx
+++ src/pages/Questionary.tsx
@@ -3,12 +3,15 @@ import Limiter from '../components/Limiter'
 import Button from '../components/Button'
 import Theme from '../components/Theme'
 import {questionsTest} from '../test/QuestionsTest'
+import Loading from '../components/Loading'
 
 function Questionary() {
 
   const [questions, setQuestions] = useState(questionsTest)
 
   const [points, setPoints] = useState(0)
+
+  const [loadingQuestionary, setLoadingQuestionary] = useState(false)
 
   const [formTransition, setFormTransition] = useState({
     render:true,
@@ -52,73 +55,86 @@ function Questionary() {
     }, 300);
   }
   
-
   return (
     <Theme>
-        <div className='min-h-screen justify-center flex'>
-            <Limiter>
-                 <div className='flex-1 flex flex-col gap-3 py-20 transition-all'>
-                    <div className='flex flex-1 gap-3'>
-                        <p className='text-white font-semibold p-3 flex-1 border-white border-2 rounded-lg'>
-                            {questions.length > 1 ?
-                                `Faltam ${questions.length} perguntas`
-                                :
-                                `Falta ${questions.length} pergunta`
-                            }
-                        </p>
-                        <p className='text-white font-semibold p-3 border-white border-2 rounded-lg bg-orange-400'>
-                            PT: 00000
-                        </p>
-                    </div>
-                    <div className={'gap-3 flex flex-col transition-all' + (formTransition.opacity?" opacity-0" : " opacity-100") }>
-                        <div className='bg-purple-600  border-white border-2 rounded-lg p-5'>
-                            <p className='text-white text-md'>
-                                {questions[0].question}
-                            </p>
-                        </div>
-                        <div className='gap-3 flex flex-col'>
-                            {
-                                questions[0].replys.map((item,index)=> 
-                                    <button
-                                        key={index}
-                                        onClick={()=>handleSelectQuestion(index)} 
-                                        className={
-                                        'border-2 transition-all border-white  p-3 text-white font-semibold rounded-lg'
-                                        + 
-                                        (
-                                            questions[0].verify 
-                                            ? 
-                                                (item.valid&&item.selected
-                                                ? " bg-emerald-400":
-                                                    item.valid&&" bg-emerald-400"||
-                                                    item.selected&& " bg-red-400"
-                                                )
-                                            :
-                                                (item.selected == false
-                                                ?   
-                                                " bg-purple-500":
-                                                " bg-orange-400")
-                                        )
-                                        }>
-                                        A) Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat fuga vitae nesciunt
-                                    </button>
-                                )
-                            }
-  
-                        </div>
+        <div className='flex justify-center items-center'>
+            <Limiter>  
+                <div className='min-h-screen justify-center flex'>
+                        <div className='flex-1 flex flex-col gap-3 py-20 transition-all'>
+                            <div className='flex flex-1 gap-3'>
+                                <p className='text-white font-semibold p-3 flex-1 border-white border-2 rounded-lg'>
+                                    {questions.length > 1 ?
+                                        `Faltam ${questions.length} perguntas`
+                                        :
+                                        `Falta ${questions.length} pergunta`
+                                    }
+                                </p>
+                                <p className='text-white font-semibold p-3 border-white border-2 rounded-lg bg-orange-400'>
+                                    PT: 00000
+                                </p>
+                            </div>
+
+
                         {
-                            !questions[0].verify? 
-                                <Button submit={handleVerifyQuestion}>
-                                    Verificar
-                                </Button>
+                            loadingQuestionary
+                            ?
+                                <div className={'gap-3 flex flex-col transition-all' + (formTransition.opacity?" opacity-0" : " opacity-100") }>
+                                    <div className='bg-purple-600  border-white border-2 rounded-lg p-5'>
+                                        <p className='text-white text-md'>
+                                            {questions[0].question}
+                                        </p>
+                                    </div>
+                                    <div className='gap-3 flex flex-col'>
+                                        {
+                                            questions[0].replys.map((item,index)=> 
+                                                <button
+                                                    key={index}
+                                                    onClick={()=>handleSelectQuestion(index)} 
+                                                    className={
+                                                    'border-2 transition-all border-white  p-3 text-white font-semibold rounded-lg'
+                                                    + 
+                                                    (
+                                                        questions[0].verify 
+                                                        ? 
+                                                            (item.valid&&item.selected
+                                                            ? " bg-emerald-400":
+                                                                item.valid&&" bg-emerald-400"||
+                                                                item.selected&& " bg-red-400"
+                                                            )
+                                                        :
+                                                            (item.selected == false
+                                                            ?   
+                                                            " bg-purple-500":
+                                                            " bg-orange-400")
+                                                    )
+                                                    }>
+                                                    A) Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat fuga vitae nesciunt
+                                                </button>
+                                            )
+                                        }
+            
+                                    </div>
+                                    {
+                                        !questions[0].verify? 
+                                            <Button submit={handleVerifyQuestion}>
+                                                Verificar
+                                            </Button>
+                                        :
+                                            <Button submit={handleNextForm}>
+                                                Continuar
+                                            </Button>
+                                    }
+                                </div> 
                             :
-                                <Button submit={handleNextForm}>
-                                    Continuar
-                                </Button>
+                            <div className='flex justify-center items-center h-full'> 
+                                <Loading></Loading>
+                            </div>
                         }
-                    </div>       
-                </div> 
-            </Limiter>              
+
+
+                        </div>           
+                </div>
+            </Limiter>
         </div>
     </Theme>
   )
